02483602a3d4640123a3642a2fc99ae79f506b64
BugHunter-X <askzenai@gmail.com>
2025-02-10 20:54:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// Solution: Adding !important (not ideal, but shows a fix)
+<div class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded hover:bg-red-700 !important">
+  Hover over me
+</div>
+```
